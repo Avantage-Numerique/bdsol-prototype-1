e64c0d7311b6ff660d4ea21b37a61bc979bb5d8c
Marc-André Martin <marcandre@mamarmite.com>
2021-10-19 17:10:41 -0400

    Ajout d'un seeder pour une classe personne.

---
+++ database/seeders/SeedOntologyClasses.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Domain\Ontology\Models\OntologyClass;
+use Illuminate\Database\Seeder;
+
+class SeedOntologyClasses extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $classes = [
+            [
+                'title' => 'Personne',
+                'slug' => 'person',
+                'intro' => 'Lorem ipsum',
+                'description' => 'Lorem ipsum',
+                'source' => ''
+            ],
+        ];
+
+        foreach($classes as $index => $onto_class_raw) {
+            $current_class = OntologyClass::create($onto_class_raw);
+        }
+    }
+}
