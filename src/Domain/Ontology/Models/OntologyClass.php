63b138a2ded49ad199c7e834b4c9445197e29167
Marc-André Martin <marcandre@mamarmite.com>
2021-11-23 17:40:08 -0500

    Ontologie UI admin ajusté et préparé pour les utilisateurs. Toujours en WIP

--- src/Domain/Ontology/Models/OntologyClass.php
+++ src/Domain/Ontology/Models/OntologyClass.php
@@ -4,30 +4,40 @@ namespace Domain\Ontology\Models;
 
 use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Domain\Ontology\Models\Traits\AvailableToApi;
+use Domain\Ontology\Models\Traits\PropertyableTrait;
+use Domain\Uri\Models\Traits\SluggableTrait;
 use Illuminate\Database\Eloquent\Model;
 use Mamarmite\Database\Traits\SecondaryDBTrait;
 
 class OntologyClass extends Model
 {
     use CrudTrait;
-    use AvailableToApi;
+    use SluggableTrait;
     use SecondaryDBTrait;
 
+    use PropertyableTrait;
+
     protected $connection = 'ontology';
+
+
+    protected $slug_from = 'title';
 
     protected $table = 'classes';
     // protected $primaryKey = 'id';
     // public $timestamps = false;
     protected $guarded = ['id'];
-    // protected $fillable = [];
+    protected $fillable = [
+        'title',
+        'slug',
+        'intro',
+        'description',
+        'source',
+        'all_properties'
+    ];
     // protected $hidden = [];
     // protected $dates = [];
 
 
     //  Relation
-
-    /*public function properties() {
-
-    }*/
 
 }
